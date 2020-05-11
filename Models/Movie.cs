8a3be73274282e81690e5d5d80e0e280ea577a33
sezgin <szgnmstfa@gmail.com>
2020-05-11 23:00:48 +0300

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuditoriskaMvc1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string DownloadURL { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
